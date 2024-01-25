e1f95f70500819942387c0607a246663bcedc6e5
Wiktoria <wiktoria@Wiktorias-MacBook-Air.local>
2024-01-25 20:49:18 +0100

    added all categories sections

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,33 +2,31 @@ import React from 'react';
 
 interface CategoriesProps {
   title: string;
-  longText: string;
+  description: string;
   imageSources: string[];
 }
 
-const Categories: React.FC<CategoriesProps> = ({ title, longText, imageSources }) => {
+const Categories: React.FC<CategoriesProps> = ({ title, description, imageSources }) => {
   return (
-    <div className="bg-gradient-to-r from-black via-zinc-600 to-zinc-300">
-    <div className="flex flex-col md:flex-row items-center justify-center px-4 md:px-16 py-8 md:py-16 min-h-max lg:h-[45rem]">
+    <div id={title} className="flex flex-col md:flex-row items-center justify-center px-4 md:px-16 py-8 md:py-20 min-h-max lg:h-[45rem]">
       <div className="md:w-2/5 md:pr-8 h-full flex flex-col justify-between">
         <div className='text-center text-white'>
           <h2 className="text-3xl font-semibold mb-6">{title}</h2>
-          <p className="text-xl font-light leading-8">{longText}</p>
+          <p className="text-xl font-light leading-8">{description}</p>
         </div>
         <div className="mt-4 bg-black text-white py-2 text-center">
-          <button className="w-full text-2xl">Kontakt</button>
+          <button className="w-full text-2xl font-light">Kontakt</button>
         </div>
       </div>
       <div className="md:w-3/5 mt-8 md:mt-0 h-full flex items-center">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
           {imageSources.map((src, index) => (
             <div key={index} className="w-full h-full overflow-hidden">
-              <img src={src} alt={`Category ${index + 1}`} className="w-full h-full object-cover" />
+              <img src={src} alt={`Image for ${title} ${index + 1}`} className="w-full h-full object-cover" />
             </div>
           ))}
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,38 +10,38 @@ const Hero: React.FC = () => {
             <div className="w-full pt-6 pb-2 px-4 md:px-10 flex flex-col md:flex-row">
                 <div className='basis-1/2 mb-6 md:mb-0'>
 
-                    <Link href="/" className="">
+                    <Link href="/">
                         <img src="/logo.png" className="max-h-[20rem]" alt="Logo" />
                     </Link>
                 </div>
                 <div className='basis-1/2'>
                     <ul className="font-regular md:font-medium flex flex-col p-1 space-y-[0.9rem] md:space-y-4 rtl:space-x-reverse mt-0 text-right text-[1.5rem] md:text-[1.6rem] leading-8">
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Ortofotomapy</a>
+                            <a href="#Ortofotomapy" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Ortofotomapy</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Modele 3D</a>
+                            <a href="#Modele 3D" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Modele 3D</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Chmury punktów</a>
+                            <a href="#Chmury punktów" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Chmury punktów</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Monitoring w czasie</a>
+                            <a href="#Monitoring w czasie" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Monitoring w czasie</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Inspekcje miejsc trudno dostępnych</a>
+                            <a href="#Inspekcje miejsc trudno dostępnych" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Inspekcje miejsc trudno dostępnych</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Pomiary mas ziemnych</a>
+                            <a href="#Pomiary mas ziemnych" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Pomiary mas ziemnych</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Przekroje poprzeczne i podłużne</a>
+                            <a href="#Przekroje poprzeczne i podłużne" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Przekroje poprzeczne i podłużne</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Dokumentacja i opracowania GIS</a>
+                            <a href="#Dokumentacja i opracowania GIS" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Dokumentacja i opracowania GIS</a>
                         </li>
                         <li>
-                            <a href="#" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Zdjęcia i filmy</a>
+                            <a href="#Zdjęcia i filmy" className='hover:text-neutral-700 text-neutral-900' aria-current="page">Zdjęcia i filmy</a>
                         </li>
                     </ul>
                 </div>
