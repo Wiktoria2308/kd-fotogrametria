6a006bd6fa6e29bd331614d9ed4d1023da335abf
Wiktoria <wiktoria@Wiktorias-MacBook-Air.local>
2024-01-27 20:21:28 +0100

    added possibilty to show videos also

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,47 +9,92 @@ export default function Home() {
   const categoriesOrtofotomapy = {
     title: "Ortofotomapy",
     description: "prezentują aktualne odzwierciedlenie sytuacji panującej w terenie. Stanowią praktyczną bazę do wszelkiego rodzaju analiz przestrzennych, planowania, projektowania, szacowania strat lub porównywania stanu terenu w różnych okresach czasu. Gotowy produkt jest w pełni kompatybilny z programami typu CAD. Wysokorozdzielcze ortofotomapy wykonywane w KD Fotogrametria odzwierciedlają szczegóły terenowe nawet na poziomie poniżej 1 centymetra. Dzięki zastosowaniu modułu RTK przy pozyskiwaniu danych otrzymujemy opracowanie o georeferencji na poziomie 3-5 centymetrów.",
-    imageSources: ["ortofotomapa1.jpeg", "ortofotomapa2.jpeg", "ortofotomapa3.jpeg", "ortofotomapa4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "ortofotomapa1.jpeg" },
+      { type: 'image', source: "ortofotomapa2.jpeg" },
+      { type: 'image', source: "ortofotomapa3.jpeg" },
+      { type: 'image', source: "ortofotomapa4.jpg" },
+    ],
   };
   const categoriesModele3D = {
     title: "Modele 3D",
     description: "wiernie odwzorowują wymiary, kształty oraz proporcje. Na ich podstawie można uzyskać wiele cennych informacji na przykład na temat budynku czy innego obiektu. Model 3D stanowi opracowanie pomocne w analizie stanu obiektu - spękania, szczeliny, ubytki. Na jego podstawie można tworzyć przekroje, dokumentacje architektoniczne, analizy obiektów przed pracami rekonstrukcyjnymi czy rozbiórkowymi, wizualizacje zabytków i innych obiektów. Stanowią również idealne opracowanie w przypadku inspekcji obiektów wysokich jak kominy lub wysokie budynki.",
-    imageSources: ["model3D_1.png", "model3D_2.jpg", "model3D_3.jpg", "model3D_4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "model3D_1.png" },
+      { type: 'image', source: "model3D_2.jpg" },
+      { type: 'image', source: "model3D_3.jpg" },
+      { type: 'image', source: "model3D_4.jpg" },
+    ],
   }
   const categoriesChmuryPunktów = {
     title: "Chmury punktów",
     description: "odwzorowują mierzone powierzchnie za pomocą milionów punktów o dokładnie obliczonych współrzędnych przestrzennych. Stanowią podstawę do dalszego projektowania czy modelowania 3D. Na ich podstawie można również dokonywać analiz terenu takich jak pomiary odległości, powierzchni, spadków i przewyższeń terenu a także generować przekroje poprzeczne i podłużne terenu. Chmura jest produktem wyjściowym do dalszych opracowań fotogrametrycznych natomiast już sama w sobie stanowi bardzo wartościowy produkt.",
-    imageSources: ["chmura1.png", "chmura2.jpg", "chmura3.jpg", "chmura4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "chmura1.png" },
+      { type: 'image', source: "chmura2.jpg" },
+      { type: 'image', source: "chmura3.jpg" },
+      { type: 'image', source: "chmura4.jpg" },
+    ],
   }
   const categoriesMonitoring = {
     title: "Monitoring w czasie",
     description: "wykorzystywany jest głównie podczas procesu budowlanego placów budowy, obiektów wielkopowierzchniowych, na żwirowniach czy obiektach, gdzie sytuacja terenowa często ulega zmianom. Można go wykonywać z różną częstotliwością w zależności od zróżnicowania terenu lub upodobań Klienta. Produkty końcowe przy monitoringu to przede wszystkim wysoko rozdzielcze ortofotomapy, modele 3D oraz zdjęcia z powietrza. Każde z opracowań jest wykonywane z najwyższą dokładnością, przez co na ich podstawie można kontrolować najmniejsze szczegóły terenowe.",
-    imageSources: ["monitoring1.jpg", "monitoring2.jpg", "monitoring3.jpg", "monitoring4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "monitoring1.jpg" },
+      { type: 'image', source: "monitoring2.jpg" },
+      { type: 'image', source: "monitoring3.jpg" },
+      { type: 'image', source: "monitoring4.jpg" },
+    ],
   }
   const categoriesInspekcje = {
     title: "Inspekcje miejsc trudno dostępnych",
     description: "z pomocą bezzałogowego statku powietrznego są bardzo przydatnym rozwiązaniem w przypadku obiektów niebezpiecznych, wysokich lub ciężko dostępnych dla ludzi. Najczęściej wykorzystywane są do kontroli stanu turbin wiatrowych, kominów, masztów TBS lub słupów energetycznych o wysokim napięciu. Takie rozwiązanie technologiczne pozwala na bezinwazyjne uzyskanie informacji o stanie infrastruktury, bez konieczności każdorazowego angażowania sprzętu wysokościowego oraz narażania zdrowia i życia osób pracujących na wysokościach.",
-    imageSources: ["inspekcje1.jpg", "inspekcje2.jpg", "inspekcje3.jpg", "inspekcje4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "inspekcje1.jpg" },
+      { type: 'image', source: "inspekcje2.jpg" },
+      { type: 'image', source: "inspekcje3.jpg" },
+      { type: 'image', source: "inspekcje4.jpg" },
+    ],
   }
   const categoriesPomiary = {
     title: "Pomiary mas ziemnych",
     description: "to rozwiązanie w szczególności przydatne w kopalniach odkrywkowych, żwirowniach, wysypiskach śmieci ale nie tylko. Podczas prowadzenia prac budowlanych na obiektach drogowych równie często wykonuje się pomiary mas ziemnych. Jest to rozwiązanie niezwykle dokładne, ponieważ błędy szacowane są na poziomie około 1%. Dzięki zastosowaniu technologii fotogrametrii niskiego pułapu nie jest konieczne wstrzymywanie prac na danym obiekcie aby wykonać pomiar. Dron zbiera dane maksymalnie w kilkadziesiąt minut a opracowane dane możesz otrzymać nawet tego samego dnia, w którym wykonywany był pomiar.",
-    imageSources: ["pomiary_mas1.jpg", "pomiary_mas2.jpg", "pomiary_mas3.jpg", "pomiary_mas4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "pomiary_mas1.jpg" },
+      { type: 'image', source: "pomiary_mas2.jpg" },
+      { type: 'image', source: "pomiary_mas3.jpg" },
+      { type: 'image', source: "pomiary_mas4.jpg" },
+    ],
   }
   const categoriesPrzekroje = {
     title: "Przekroje poprzeczne i podłużne",
     description: "obrazują ukształtowanie terenu szczególnie przydatne w budownictwie, architekturze oraz geologii. Możliwość generowania przekrojów na podstawie danych fotogrametrycznych pozwala na przeprowadzenie szczegółowych analiz terenów rozległych na przykład pod projekty drogowe, kolejowe lub do wyznaczania zlewni w opracowaniach hydrologicznych.",
-    imageSources: ["przekrój1.jpg", "przekrój2.jpg", "przekrój3.jpg", "przekrój4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "przekrój1.jpg" },
+      { type: 'image', source: "przekrój2.jpg" },
+      { type: 'image', source: "przekrój3.jpg" },
+      { type: 'image', source: "przekrój4.jpg" },
+    ],
   }
   const categoriesDokumentacjaGIS = {
     title: "Dokumentacja i opracowania GIS",
     description: "prezentują wyniki badań i analiz przestrzennych wykonywanych na poszczególnych opracowaniach fotogrametrycznych. Do tej kategorii danych zaliczyć można przede wszystkim obliczenia powierzchni upraw rolnych zniszczonych w wyniku niekorzystnych warunków pogodowych lub działalności zwierząt. Dokumentacja w postaci ortofotomapy jest podstawą do graficznej prezentacji uszkodzenia. Dane fotogrametryczne można z powodzeniem łączyć z innymi danymi przestrzennymi takimi jak na przykład granice działek ewidencyjnych czy budynki ewidencyjne.",
-    imageSources: ["analiza_gis1.png", "analiza_gis2.jpg", "analiza_gis3.jpg", "analiza_gis4.jpeg"],
+    mediaSources: [
+      { type: 'image', source: "analiza_gis1.png" },
+      { type: 'image', source: "analiza_gis2.jpg" },
+      { type: 'image', source: "analiza_gis3.jpg" },
+      { type: 'image', source: "analiza_gis4.jpeg" },
+    ],
   }
   const categoriesZdjeciaIFilmy = {
     title: "Zdjęcia i filmy",
     description: "z lotu ptaka stanowią oryginalne i bardzo atrakcyjne rozwiązanie do wizualizacji terenów rekreacyjnych, osiedli mieszkaniowych lub nieruchomości przeznaczonych do sprzedaży. Zdjęcia i filmy mają ogromny potencjał także jako podstawowy monitoring czy inspekcja, ponieważ pozwalają na uchwycenie istotnych szczegółów terenowych lub udokumentowanie aktualnego stanu obiektu lub terenu.",
-    imageSources: ["zdjęcie1.jpg", "zdjęcie2.jpg", "zdjęcie3.jpg", "zdjęcie4.jpg"],
+    mediaSources: [
+      { type: 'image', source: "zdjęcie1.jpg" },
+      { type: 'image', source: "zdjęcie2.jpg" },
+      { type: 'image', source: "zdjęcie3.jpg" },
+      { type: 'image', source: "zdjęcie4.jpg" },
+    ],
   }
 
   return (
--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 interface CategoriesProps {
   title: string;
   description: string;
-  imageSources: string[];
+  mediaSources: { type: string; source: string }[];
 }
 
-const Categories: React.FC<CategoriesProps> = ({ title, description, imageSources }) => {
+const Categories: React.FC<CategoriesProps> = ({ title, description, mediaSources }) => {
   return (
     <div id={title} className="flex flex-col md:flex-row items-center justify-center px-4 md:px-16 py-8 md:py-20 min-h-max lg:h-[45rem]">
       <div className="md:w-2/5 md:pr-8 h-full flex flex-col justify-between">
@@ -22,11 +22,19 @@ const Categories: React.FC<CategoriesProps> = ({ title, description, imageSource
       </div>
       <div className="md:w-3/5 mt-8 md:mt-0 h-full flex items-center">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
-          {imageSources.map((src, index) => (
-            <div key={index} className="w-full h-full overflow-hidden">
-              <img src={src} alt={`Image for ${title} ${index + 1}`} className="w-full h-full object-cover" />
-            </div>
-          ))}
+        {mediaSources.map((media, index) => (
+              <div key={index} className="w-full h-full overflow-hidden">
+                {media.type === 'image' ? (
+                  <img src={media.source} alt={`Category ${index + 1}`} className="w-full h-full object-cover" />
+                ) : media.type === 'video' ? (
+                  <video controls className="w-full h-full">
+                    <source src={media.source} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                ) : null}
+
+              </div>
+            ))}
         </div>
       </div>
     </div>
