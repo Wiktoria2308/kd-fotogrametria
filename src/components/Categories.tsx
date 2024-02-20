63896578c64a6173648d5d5c33398412214bc94e
Wiktoria <wiktoria@Wiktorias-MacBook-Air.local>
2024-02-20 21:12:10 +0100

    deleted unsed code

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -9,16 +9,12 @@ interface CategoriesProps {
 
 const Categories: React.FC<CategoriesProps> = ({ title, description, image }) => {
 
-
-
   return (
-    <div className={`xl:relative h-full xl:min-h-screen mb-36 ${title}`}>
+    <div className={`xl:relative h-full xl:min-h-screen mb-36 ${title}`} id={title}>
       <div className="xl:absolute xl:inset-0 bg-cover bg-center h-[300px] md:h-[500px] xl:h-full" style={{ backgroundImage: `url(${image})` }}></div>
-  
       <div className={`xl:inset-y-0 xl:right-0 xl:absolute w-full xl:w-[35%] ${window.innerWidth >= 1280 ? 'bg-blur-right' : 'bg-zinc-300'} flex flex-col xl:justify-center`}>
         <div className="text-black px-6 lg:px-10 py-10 lg:min-h-[unset] categories-info">
           <h2 className="text-xl md:text-xl lg:text-2xl 2xl:text-3xl font-medium mb-5 lg:mb-6 2xl:mb-10 text-center">{title}</h2>
-          {/* <p className="text-lg md:text-lg/9 2xl:text-2xl/10 font-normal text-left lg:text-center">{description}</p> */}
           <div className="overflow-auto xl:max-h-[400px] 2xl:max-h-[unset] description">
             <p className="text-lg md:text-lg/9 2xl:text-2xl/10 font-normal text-left lg:text-center">{description}</p>
           </div>
